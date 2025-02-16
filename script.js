711f142cae77f65e7ebb88d95629ecc822566a86
AaryaB6 <aaryaborkar67@gmail.com>
2025-02-16 23:41:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*Dark Mode*/
+$( ".change" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+        $( "body" ).removeClass( "dark" );
+        $( ".change" ).text( "OFF" );
+    } else {
+        $( "body" ).addClass( "dark" );
+        $( ".change" ).text( "ON" );
+    }
+});
