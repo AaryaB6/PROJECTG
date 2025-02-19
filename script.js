f6d67278c8de03d280dc17cd87c2ce728609733e
AaryaB6 <aaryaborkar67@gmail.com>
2025-02-19 12:28:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ $( ".change" ).on("click", function() {
         $( ".change" ).text( "ON" );
     }
 });
+/*Home Slider*/
+let swiper = new Swiper(".home-slider", {
+    autoplay: {
+        delay:3000,
+        disableOnInteraction: false,
+    },
+    pagination:{
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+    loop: true,  
+});
+
+/*About */
+var slideIndex = 0;
+slowSlides();  // Call the correct function
+
+function slowSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) { 
+        slideIndex = 1; 
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(slowSlides, 2000);  // Corrected function call
+}
